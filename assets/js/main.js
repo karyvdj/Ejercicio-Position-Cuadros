03b86e40b850d79b7671ec7d9908b28eefd62311
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-18 18:30:02 -0500

    Listo en HTML y CSS, creacion dinamica de los elementos

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// alert("hola");
+var contenedor = document.getElementById('contenedor')
+var divabuelo = document.createElement("div");
+var divPadre = document.createElement("div");
+var divHijo= document.createElement("div");
+
+contenedor.appendChild("divabuelo");
